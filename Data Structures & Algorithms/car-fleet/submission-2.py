a8507988d2f72a1a13863f858e936e0920fd3345
ChandlerBone <55212897+Chandler-Bone@users.noreply.github.com>
2026-06-26 17:31:01 -0400

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        fleets = 0
+        pos_ctime = []
+
+        for i in range(len(position)):
+            ctime = (target - position[i]) / speed[i]
+            pos_ctime.append((position[i],ctime))
+        
+        pos_ctime = sorted(pos_ctime)
+
+        max_val = float('-inf')
+        print(pos_ctime)
+        for i in range(len(pos_ctime) - 1, -1 , -1):
+            if(pos_ctime[i][1] > max_val):
+                max_val = pos_ctime[i][1]
+                fleets += 1
+            
+        return fleets
