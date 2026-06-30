464d253659fe0ec3619361473d9e7c9f1f2db214
ChandlerBone <55212897+Chandler-Bone@users.noreply.github.com>
2026-06-29 23:31:06 -0400

    Add: time-based-key-value-store - submission-16

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-16.py
@@ -0,0 +1,31 @@
+class TimeMap:
+
+    def __init__(self):
+        self.names = defaultdict(list)
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.names[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        name_vals = self.names[key]
+
+        l = 0
+        r = len(name_vals) - 1
+
+        res = ""
+        print(name_vals)
+        while l <= r:
+            m = l + (r - l) // 2
+            print(l)
+            print(r)
+            print(m)
+            print("+++")
+
+            if timestamp >= name_vals[m][0]:   
+                res = name_vals[m][1]
+                l = m + 1
+            else:
+                r = m - 1
+
+        return res
