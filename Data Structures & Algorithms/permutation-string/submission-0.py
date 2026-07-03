fbd614d4e0cdca2c5ff217d5e99ae40d233901e3
ChandlerBone <55212897+Chandler-Bone@users.noreply.github.com>
2026-07-03 17:49:29 -0400

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        char_list1 = [0] * 26
+        char_list2 = [0] * 26
+
+        if len(s2) < len(s1):
+            return False
+
+        for i in s1:
+            char_list1[ord(i) - ord('a')] += 1
+
+        for r in range(len(s2)):
+            l = r - (len(s1) - 1) if r >= len(s1) - 1 else 0
+            
+            char_list2[ord(s2[r]) - ord('a')] += 1
+            if l > 0:
+                char_list2[ord(s2[l - 1]) - ord('a')] -= 1
+
+            if char_list1 == char_list2:
+                return True
+
+            # if :
+            #     l += 1
+
+        return False
+
+        
+
+
+        
