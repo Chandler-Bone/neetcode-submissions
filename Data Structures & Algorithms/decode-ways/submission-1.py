f25b5bb6b1dcd48ebdbf9a77ace9e33e57577536
ChandlerBone <55212897+Chandler-Bone@users.noreply.github.com>
2026-09-01 22:09:33 -0400

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+
+        dp = {len(s) : 1}
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if s[i] == "0":
+                return 0
+
+            count = dfs(i + 1)
+            if len(s) - i >= 2 and int(s[i:i+2]) <= 26:
+                count += dfs(i + 2)
+
+            dp[i] = count
+            return count
+
+
+        return dfs(0)
+
+        
