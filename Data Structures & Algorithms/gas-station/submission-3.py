34ca93213a4f9bff649c9b83f83e5c15654aba28
ChandlerBone <55212897+Chandler-Bone@users.noreply.github.com>
2026-09-16 19:18:45 -0400

    Add: gas-station - submission-3

---
+++ Data Structures & Algorithms/gas-station/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
+    #[-1, 0, -1, 3]
+
+    #[-1, -1, -1]
+
+        ev = []
+        
+        for i in range(len(gas)):
+            ev.append((gas[i] - cost[i]))
+
+        # print(ev)
+
+        if sum(ev) < 0:
+            return -1
+
+        curr_val = 0
+        max_val = float('-inf')
+        max_idx = -1
+        for i in range(len(ev) - 1, -1, -1):
+            curr_val += ev[i]
+            if curr_val > max_val:
+                max_idx = i
+                max_val = curr_val
+
+        return max_idx
